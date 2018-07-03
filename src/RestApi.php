85a021956dfdb10fd18a6b755e246ebecdcf3ae6
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-07-03 06:41:23 +0100

    generate Angular model and components

---
+++ src/RestApi.php
@@ -0,0 +1,26 @@
+<?php
+namespace Cig;
+// use Symfony\Component\Cache\Simple\FilesystemCache;
+class RestApi {
+
+  function __construct($baseUrl, \Psr\SimpleCache\CacheInterface $cache){
+    $this->baseUrl = $baseUrl;
+    $this->cache = $cache;
+  }
+
+  function query($entity, $action, $params = [], $cache = true) {
+    $key = md5(serialize([$entity, $action, $params]));
+    if($cache && substr($action, 0, 3) && $this->cache->hasItem($key)){
+      return $this->cache->get($key);
+    }
+    $json = json_encode($params);
+    $result = json_decode(file_get_contents(
+      $this->baseUrl .
+        "&entity={$entity}" .
+        "&action={$action}" .
+        "&json={$json}"
+    ),1);
+    $this->cache->set($key, $result);
+    return $result;
+  }
+}
