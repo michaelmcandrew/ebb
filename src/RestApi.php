dc453c773829dfa63e6ac9ef8d2d0ad0784646c9
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-08-02 23:27:52 +0100

    rest api client uses guzzle

--- src/RestApi.php
+++ src/RestApi.php
@@ -1,26 +1,38 @@
 <?php
 namespace Ebb;
-// use Symfony\Component\Cache\Simple\FilesystemCache;
-class RestApi {
 
-  function __construct($baseUrl, \Psr\SimpleCache\CacheInterface $cache){
-    $this->baseUrl = $baseUrl;
-    $this->cache = $cache;
-  }
+use Symfony\Component\Cache\Simple\FilesystemCache;
+use GuzzleHttp\Client;
 
-  function query($entity, $action, $params = [], $cache = true) {
-    $key = md5(serialize([$entity, $action, $params]));
-    if($cache && substr($action, 0, 3) && $this->cache->hasItem($key)){
-      return $this->cache->get($key);
+class RestApi
+{
+    public function __construct($baseUrl, $cache, $flush = false)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->cache = $cache;
+        $this->flush = $flush;
     }
-    $json = json_encode($params);
-    $result = json_decode(file_get_contents(
-      $this->baseUrl .
-        "&entity={$entity}" .
-        "&action={$action}" .
-        "&json={$json}"
-    ),1);
-    $this->cache->set($key, $result);
-    return $result;
-  }
+
+    public function query($entity, $action, $params = [])
+    {
+        $key = md5(serialize([$entity, $action, $params]));
+        if (!$this->flush && substr($action, 0, 3) == 'get' && $this->cache->hasItem($key)) {
+            return $this->cache->get($key);
+        }
+        $json = json_encode($params);
+        $client = new Client(['http_errors' => false]);
+        $url = $this->baseUrl . "&entity={$entity}&action={$action}&json={$json}";
+        $response = $client->request('GET', $url);
+        if ($response->getStatusCode() >= 400) {
+            $errorMessage = "API error: {$response->getStatusCode()}: {$response->getReasonPhrase()} ($url)";
+            throw new \Exception($errorMessage);
+        }
+        $result = json_decode($response->getBody(), 1);
+        if ($result['is_error']) {
+            $errorMessage = "API error: {$result['error_message']}";
+            throw new \Exception($errorMessage);
+        }
+        $this->cache->set($key, $result);
+        return $result;
+    }
 }
