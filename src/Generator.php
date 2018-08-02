c907825fa0c5c67f23b1633203212187c6b09d8a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-08-02 23:29:43 +0100

    don't create interfaces, just json schema

--- src/Generator.php
+++ src/Generator.php
@@ -1,38 +1,69 @@
 <?php
 namespace Ebb;
 
-use \Twig_Environment;
+use \Psr\Log\LoggerInterface;
 
 class Generator
 {
-    public function __construct(Twig_Environment $twig)
+    /**
+     * entities to be used in interface generation
+     * @var Entity[]
+     */
+    public $entities = [];
+
+    private $api;
+
+    private $log;
+
+    public function __construct(RestApi $api, LoggerInterface $log)
     {
-        $this->twig = $twig;
+        $this->api = $api;
+        $this->log = $log;
     }
 
-    public function init($templateDefinitions, $outputDir)
+    /**
+     * loads entities from the API and adds valid entities to the
+     * $this->entities array
+     */
+    public function run()
     {
-        $this->outputDir = $outputDir;
-        foreach ($templateDefinitions as $key => $def) {
-            $this->definitions[$key]['filename'] = $this->twig->createTemplate($def['filename']);
-            $this->definitions[$key]['template'] = $this->twig->load($def['template']);
+        // Query API for valid entities
+        $entityGet = $this->api->query('Entity', 'get');
+        $entityList = array_diff($entityGet['values'], $entityGet['deprecated']);
+        foreach ($entityList as $entityName) {
+            $fields = $this->api->query($entityName, 'getfields')['values'];
+            $entities[$entityName]['fields'] = $fields;
+
+            // Find many to one relationships
+            foreach ($fields as $field) {
+                if (!empty($field['FKApiName'])) {#
+                    $entities[$entityName]['rels']['mto'][$field['name']] = $field['FKApiName'];
+                }
+            }
+        }
+
+        // Record corresponding one to many relationships
+        foreach ($entities as $entityName => $entity) {
+            if (!empty($entity['rels']['mto'])) {
+                foreach ($entity['rels']['mto'] as $field => $relatedEntity) {
+                    $entities[$relatedEntity]['rels']['otm'][$entityName][] = $field;
+                }
+            }
+        }
+
+        // Create entity objects and add them to $this->entities
+        foreach ($entities as $entityName => $entity) {
+            try {
+                $entity = new Entity($entityName, $entity, $this->log);
+                $this->entities[$entity->names['kebab']] = $entity;
+            } catch (\Exception $e) {
+                $this->log->notice($e->getMessage());
+            }
         }
     }
 
-    public function generate($key, $params)
+    public function dump()
     {
-        $filename = $this->getTemplateFilename($key, $params);
-        $output = $this->getTemplateOutput($key, $params);
-        file_put_contents($filename, $output);
-    }
-
-    protected function getTemplateFilename($key, $params)
-    {
-        return $this->outputDir . '/' .  $this->definitions[$key]['filename']->render($params);
-    }
-
-    protected function getTemplateOutput($key, $params)
-    {
-        return $this->definitions[$key]['template']->render($params);
+        return json_encode($this, JSON_PRETTY_PRINT);
     }
 }
