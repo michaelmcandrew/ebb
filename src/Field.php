3cbc0931087824f6c8d0872403ec2fd398b2e107
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-08-03 21:58:14 +0100

    simplify field generation

--- src/Field.php
+++ src/Field.php
@@ -5,28 +5,17 @@ use Illuminate\Support\Str;
 
 class Field
 {
-
-    private $entity;
-
     public function __construct($original, Entity $entity)
     {
         $this->name = $original['name'];
-        $this->entity = $entity;
         $this->type = CiviTypes::$translation[$original['type']];
         if (isset($original['label'])) {
-            $label = ucfirst(strtolower($original['label']));
+            $this->label = ucfirst(strtolower($original['label']));
         } elseif (isset($original['title'])) {
-            $label = ucfirst(strtolower($original['title']));
+            $this->label = ucfirst(strtolower($original['title']));
         } else {
-            throw new \Exception("Could not find label for {$original['name']} ({$entity->names['original']})");
+            throw new \Exception("Could not find label for {$original['name']} ({$entity->name})");
         }
-        if (strpos($label, $entity->names['sentence']) === 0) {
-            $label = ucfirst(trim(substr($label, strlen($entity->names['sentence']))));
-        }
-        if ($label == '') {
-            $label = 'Name';
-        }
-        $this->label = $label;
     }
 
     public function getName()
