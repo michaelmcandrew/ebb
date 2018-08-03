2cab8deb2a07c41a77601be00f33c161ce7972bd
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-08-03 21:58:43 +0100

    remove references to ->names[]

--- src/Entity.php
+++ src/Entity.php
@@ -19,22 +19,22 @@ class Entity
     {
         $this->original = $entity;
         if (empty($entity['fields'])) {
-            throw new \Exception("No fields defined for entity {$this->names['original']}.");
+            throw new \Exception("No fields defined for entity {$this->name}.");
         }
         foreach ($entity['fields'] as $original) {
             try {
-                if (!empty($original['entity']) && $original['entity'] == $this->names['original']) {
+                if (!empty($original['entity']) && $original['entity'] == $this->name) {
                     $field = new Field($original, $this);
                     $this->fields[$field->getName()] = $field;
                 } else {
-                    throw new \Exception("No matching entity field for {$original['name']} ({$this->names['original']}).");
+                    throw new \Exception("No matching entity field for {$original['name']} ({$this->name}).");
                 }
             } catch (\Exception $e) {
                 $this->log->info($e->getMessage());
             }
         }
         if (!isset($this->fields)) {
-            throw new \Exception("No fields with 'entity' defined for entity {$this->names['original']}.");
+            throw new \Exception("No fields with 'entity' defined for entity {$this->name}.");
         }
     }
 
